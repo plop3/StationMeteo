dfe3a1997488012a504bf7ca8414c4ce746e9b00
Plop3 <serge@plop3.fr>
2020-02-13 14:40:03 +0100

    Add files via upload

--- Fonctions.ino
+++ Fonctions.ino
@@ -1,18 +1,19 @@
+// Récupération des données des capteurs
 void infoMeteo() {
   // BME280
-  Gval[0] = round(bme.readTemperature() * 10) / 10.0;
-  Gval[1] = bme.readPressure() / 100.0F;
-  Gval[2] = bme.readHumidity();
+  Gtemp = round(bme.readTemperature() * 10) / 10.0;
+  Gpress = bme.readPressure() / 100.0F;
+  Ghum = bme.readHumidity();
 #if defined(DEBUG)
   Serial.print("Temperature = ");
-  Serial.print(Gval[0]);
+  Serial.print(Gtemp);
   Serial.println(" *C");
   Serial.print("Pressure = ");
-  Serial.print(Gval[1]);
+  Serial.print(Gpress);
   Serial.print(" "); Serial.print(weather[forecast]);
   Serial.println(" hPa");
   Serial.print("Humidity = ");
-  Serial.print(Gval[2]);
+  Serial.print(Ghum);
   Serial.println(" %");
 #endif
 	// Pluie (WS2300)
@@ -22,7 +23,7 @@ void infoMeteo() {
 	if(code == HTTP_CODE_OK) {
 		String payload = http.getString();
 		deserializeJson(doc, payload);
-		Gval[3]=doc["result"][0]["RainRate"];
+		Grain=doc["result"][0]["RainRate"];
 	
 		http.end();
 		// Vent (WS2300)
@@ -31,14 +32,16 @@ void infoMeteo() {
 		if(code == HTTP_CODE_OK) {
 			String payload = http.getString();
 			deserializeJson(doc, payload);
-			Gval[5]=doc["result"][0]["Speed"];
+			Gvent=doc["result"][0]["Speed"];
+			if (Gvent==59) Gvent=0;	// Bug de l'anémomètre
 			http.end();
 			http.begin("http://192.168.0.7:8080/json.htm?type=devices&rid=3466");
 			code=http.GET();
 			if(code == HTTP_CODE_OK) {
 				String payload = http.getString();
 				deserializeJson(doc, payload);
-				Gval[6]=doc["result"][0]["Gust"];
+				Graf=doc["result"][0]["Gust"];
+				if (Graf==59) Graf=0;	// Bug de l'anémomètre
 			}
 		}
 	}
@@ -48,58 +51,94 @@ void infoMeteo() {
   float tempMlx=mlx.readAmbientTempC();
   float stateciel;
   double Tcloudy = CLOUD_TEMP_OVERCAST, Tclear = CLOUD_TEMP_CLEAR;
-  //double Td = (K1 / 100.) * (Gval[0] - K2 / 10) + (K3 / 100.) * pow((exp (K4 / 1000.* Gval[0])) , (K5 / 100.));
+  //double Td = (K1 / 100.) * (Gtemp - K2 / 10) + (K3 / 100.) * pow((exp (K4 / 1000.* Gtemp)) , (K5 / 100.));
   double Td = (K1 / 100.) * (tempMlx - K2 / 10) + (K3 / 100.) * pow((exp (K4 / 1000.* tempMlx)) , (K5 / 100.));
   tempciel=tempciel-Td;
-  Serial.print("T° ciel: ");Serial.println(tempciel);
-  Gval[7]=tempciel;
+  //Serial.print("T° ciel: ");Serial.println(tempciel);
+  GTciel=tempciel;
   Clouds = cloudIndex(tempciel);
   if (tempciel<Tclear) stateciel=Tclear;
   if (tempciel>Tcloudy) stateciel=Tcloudy; 
-  CouvNuages=(stateciel-Tclear)*100/(Tcloudy-Tclear);
-  Serial.print("Couverture nuageuse"+String(CouvNuages));
-  if (CouvNuages>CLOUD_FLAG_PERCENT) {
+  Gnuage=(stateciel-Tclear)*100/(Tcloudy-Tclear);
+  //Serial.print("Couverture nuageuse"+String(Gnuage));
+  if (Gnuage>CLOUD_FLAG_PERCENT) {
     cloudy=1;
   } else {
     cloudy=0;
   }
-  
+  // BH1750
+  Glum=lightSensor.readLightLevel();
+  // Index UV
+  float ir = uv.readIR();
+  Guv = uv.readUV()*RP_UV;
+  Guv /= 100.0;
+  // SQM
+  int count=0;
+  double sum=0;
+  bool reading=true;
+  FreqMeasure.begin();
+  while (reading) {
+	  if (FreqMeasure.available()) {
+		  sum = sum + FreqMeasure.read();
+		  count++;
+		  if (count > 30) {
+			double frequency = F_CPU / (sum / count);
+			Gsqm = A - 2.5*log10(frequency); //Frequency to magnitudes/arcSecond2 formula
+			reading=false;
+			FreqMeasure.end();
+		  }
+	  }
+  }
 }
 
 //-----------------------------------------------
-
+// Envoi des infos au serveur Domoticz
+//-----------------------------------------------
 void envoiHTTP() {
   // Baromètre
   HTTPClient http;
-  http.begin("http://192.168.0.7:8080/json.htm?type=command&param=udevice&idx=3529&nvalue=0&svalue="+String(Gval[0])+";"+String(Gval[2])+";0;"+String(Gval[1])+";"+String(forecast));
+  http.begin("http://192.168.0.7:8080/json.htm?type=command&param=udevice&idx=3529&nvalue=0&svalue="+String(Gtemp)+";"+String(Ghum)+";0;"+String(Gpress)+";"+String(forecast));
   http.GET();
   http.end();
   // T° du ciel (soit T° soit couverture nuageuse)
-  http.begin("http://192.168.0.7:8080/json.htm?type=command&param=udevice&idx=484&nvalue=0&svalue="+String(Gval[7]));
-  //http.begin("http://192.168.0.7:8080/json.htm?type=command&param=udevice&idx=484&nvalue=0&svalue="+String(CouvNuages));
+  http.begin("http://192.168.0.7:8080/json.htm?type=command&param=udevice&idx=484&nvalue=0&svalue="+String(GTciel));
+  //http.begin("http://192.168.0.7:8080/json.htm?type=command&param=udevice&idx=484&nvalue=0&svalue="+String(Gnuage));
   http.GET();
   http.end();
 }
 
+//-----------------------------------------------
+// Informations à destination du driver Indi
 //-----------------------------------------------
 void webStatus() {
   // Envoi les infos météo à Indi
   //TODO Voir pour remplacer la température du ciel par la couverture nuageuse
-  //String msg = "pluie="+String(Gval[3])+"\ntemperature=" + String(Gval[0]) + "\nvent="+String(Gval[5])+"\nrafale="+String(Gval[6])+"\nnuages="+String(50-Gval[7]+Gval[0])+"\n";
-  String msg = "pluie="+String(Gval[3])+"\ntemperature=" + String(Gval[0]) + "\nvent="+String(Gval[5])+"\nrafale="+String(Gval[6])+"\nnuages="+String(CouvNuages)+"\n";
+  //String msg = "pluie="+String(Grain)+"\ntemperature=" + String(Gtemp) + "\nvent="+String(Gvent)+"\nrafale="+String(Graf)+"\nnuages="+String(50-GTciel+Gtemp)+"\n";
+  String msg = "pluie="+String(Grain)+"\ntemperature=" + String(Gtemp) + "\nvent="+String(Gvent)+"\nrafale="+String(Graf)+"\nnuages="+String(Gpluie?120,Gnuage)+"\n";
   server.send(200, "text/plain", msg);
   //server.send(200, "application/json", msg);
 }
 
 //-----------------------------------------------
+// Lecture du capteur de pluie
+//-----------------------------------------------
+bool infoPluie() {
+	Gpluie=!digitalRead(PinPluie);
+}
+//-----------------------------------------------
+// Serveur web fournissant les infos météo
+//-----------------------------------------------
 void webMeteo() {
   // Envoi les infos météo
-  String msg = "Temperature = " + String(Gval[0]) + " C\n";
-  msg = msg + "Humidite = " + String(Gval[2]) + "%\n";
-  msg = msg + "Pression = " + String(Gval[1]) + " Hpa " + String(weather[forecast]) + "\n";
+  String msg = "Temperature = " + String(Gtemp) + " C\n";
+  msg = msg + "Humidite = " + String(Ghum) + "%\n";
+  msg = msg + "Pression = " + String(Gpress) + " Hpa " + String(weather[forecast]) + "\n";
   server.send(200, "text/plain", msg + "\n");
 }
 
+//-----------------------------------------------
+// Calcul de l'index nuageux
+//-----------------------------------------------
 double cloudIndex(double Tsky) {
   double Tcloudy = CLOUD_TEMP_OVERCAST, Tclear = CLOUD_TEMP_CLEAR;
   //double Tsky = skyTemp();
@@ -108,4 +147,49 @@ double cloudIndex(double Tsky) {
   if (Tsky > Tcloudy) Tsky = Tcloudy;
   Index = (Tsky - Tclear) * 100 / (Tcloudy - Tclear);
   return Index;
+}
+
+//-----------------------------------------------
+// Orage
+//-----------------------------------------------
+void orage() {
+  detected = true;
+}
+
+//-----------------------------------------------
+// Type de détection d'orage
+//-----------------------------------------------
+void translateIRQ(uns8 irq) {
+  switch (irq) {
+    case 1:
+      Serial.println("NOISE DETECTED");
+      break;
+    case 4:
+      Serial.println("DISTURBER DETECTED");
+      break;
+    case 8:
+      Serial.println("LIGHTNING DETECTED");
+      sendOrage();
+      break;
+  }
+}
+
+//-----------------------------------------------
+// Envoi des infos d'orage TODO
+//-----------------------------------------------
+void sendOrage() {
+  int distance = mod1016.calculateDistance();
+  int intensite = mod1016.getIntensity();
+  if (distance == -1)
+    Serial.println("Lightning out of range");
+  else if (distance == 1)
+    Serial.println("Distance not in table");
+  else if (distance == 0)
+    Serial.println("Lightning overhead");
+  else {
+    Serial.print("Lightning ~");
+    Serial.print(distance);
+    Serial.println("km away\n");
+    Serial.print("Intensité: ");Serial.println(intensite);
+  }
 }
--- StationMeteo.ino
+++ StationMeteo.ino
@@ -1,26 +1,31 @@
 /*
   Station météo V2
-  Version:  2.00.00
-  Date:     10/02/2020
+  Version:  2.00.01
+  Date:     10/02/2020 - 13/02/2020
   Auteur:   Serge CLAUS
   Licence:  GPL V3
 */
 
 //-----------------------------------------------
 // Librairies
+//-----------------------------------------------
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-
+// Timers
 #include <SimpleTimer.h>
-
+// Client http
 #include <HTTPClient.h>
 
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
+// SQM
+#include <FreqMeasure.h>
+#include <Math.h>
+// BME280
 #include <Adafruit_BME280.h>
 //Serveur Web
 #include <WebServer.h>
@@ -28,16 +33,23 @@
 #include <ArduinoJson.h>
 //MLX90614
 #include <Adafruit_MLX90614.h>
+// Mod1016
+#include <AS3935.h>
+// DHT22
+#include "DHT.h"
+// BH1750 Luminosité
+#include <BH1750.h>
+//SI1145
+#include "Adafruit_SI1145.h"
+//MLX90614
+#include <Adafruit_MLX90614.h>
 
 // Fichiers externes
 #include "StationMeteo.h"
 
-
-
 //-----------------------------------------------
-
 // Macros
-
+//-----------------------------------------------
 // BME280
 #define BME280ADDR  0x76
 #define SEALEVELPRESSURE_HPA (1020.5)
@@ -53,37 +65,73 @@
 #define CLOUD_TEMP_OVERCAST 0
 #define CLOUD_FLAG_PERCENT 30
 
-// Meteo
-#define NBC 8    // Nombre de capteurs
+// Capteur de pluie
+#define PinPluie 3	// TODO
+// DHT22
+#define DHTPIN 2	// PIN DHT22	TODO
+#define DHTTYPE DHT22	// Type DTH (11/22)
+// Luminosité
+#define ID_LIGHT 10	// Luminosité en Lux
+// Index UV
+#define ID_UV 11	// Indice UV
+#define RP_UV 1.3	// Multiplicateur index UV (du au cache dépoli)
+#define ID_IR 12
+#define ID_VI 13
+// MOD-1016
+#define ID_ORAGED 3	// Distance de l'éclair
+#define ID_ORAGEI 4	// Intensité de l'éclair
+#define ID_ORAGEC 5	// Interrupteur compteur d'orage (Envoi "On" à chaque éclair)
+#define IRQ_ORAGE 18	// PIN IRQ (18 boitier externe, 19 boitier station)
+// SQM TODO
+#define SQMPin	4	// PIN du capteur SQM
+//-----------------------------------------------
+// Activation des modules
+//-----------------------------------------------
 // WiFi
 WiFiMulti wifiMulti;
-
 //Serveur Web
 WebServer server(80);
-
 // Timer
 SimpleTimer timer;
 
+
 // BME 280
 Adafruit_BME280 bme; // I2C
-
 // MLX90614
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+// Luminosité
+BH1750 lightSensor;
+// SI1145
+Adafruit_SI1145 uv = Adafruit_SI1145();
+// DHT22
+DHT dht(DHTPIN, DHTTYPE);
+// Capteur de pluie
 
 //-----------------------------------------------
 // Variables globales
+//-----------------------------------------------
 /*
-  0:  Température
-  1:  Pression athmosphérique
-  2:  Humidité
-  3:  Pluviomètre
-  4:  Capteur de pluie
-  5:  Vent
-  6:  Rafales
-  7:  Couverture nuageuse (MLX)
+  0:  Gtemp		Température
+  1:  Gpress	Pression athmosphérique
+  2:  Ghum		Humidité
+  3:  Grain		Pluviomètre
+  4:  Gpluie	Capteur de pluie (TOR)				TODO
+  5:  Gvent		Vent
+  6:  Graf		Rafales
+  7:  Gnuage	Couverture nuageuse (MLX)
+  8:  GTciel	Température du ciel (MLX)			TODO
+  9:  Gsqm		Qualité du ciel SQM					TODO
+  10:  Guv		Index UV							TODO
+  11: Glum		Luminosité							TODO
+  12: GdistO	Distance orage						TODO
+  13: GintO		Intensité orage						TODO
+  14: GdirV		Direction du vent					TODO
+  15: GTabri	Température de l'abri	(DHT22)		TODO
+  16: GHabri	Humidité de l'abri 		(DHT22)		TODO
 */
-float Gval[NBC];        // Résultats des sondes
-//float Gold[NBC] = {99, 0, 0}; // Valeurs précédentes
+float Gtemp, Ghum, Gpress, Grain, Gvent, Graf, Gnuage, GTciel, Gsqm, Guv, Glum, GdistO, GintO, GdirV;
+bool Gpluie=false;
+
 // Chaine json
 StaticJsonDocument<2048> doc;
 
@@ -92,6 +140,8 @@ int cloudy;
 float Clouds;
 float CouvNuages;
 
+//-----------------------------------------------
+// Setup
 //-----------------------------------------------
 void setup() {
   // Port série
@@ -106,7 +156,7 @@ void setup() {
     delay(5000);
     ESP.restart();
   }
-
+//-----------------------------------------------
   // OTA
   ArduinoOTA.setHostname("stationmeteo");
   ArduinoOTA
@@ -138,23 +188,51 @@ void setup() {
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
+//-----------------------------------------------
   // Serveur Web
   server.on("/status", webStatus);
   server.on("/meteo", webMeteo);
   server.begin();
 
   // Timers
-  timer.setInterval(10000L, infoMeteo);   // Lecture des capteurs toutes les 10s
-  //timer.setInterval(300000L, infoDomo);  //Tente de se connecter au serveur MQTT toutes les 5mn et envoie les données météo au serveur domotique
-  timer.setInterval(60000L, envoiHTTP);   // Envoie les infos météo au serveur Domoticz toutes les 1 mn
+  timer.setInterval(10000L, infoPluie);   // Lecture du capteur de pluie TOR toutes les 10s
+  timer.setInterval(30000L, infoMeteo);	  // Lecture des capteurs toutes les 30s
+  timer.setInterval(180000L, envoiHTTP);   // Envoie les infos météo au serveur Domoticz toutes les 3 mn
 
   // BME280
   bme.begin(0x76);
   //MLX (Ciel) 
   mlx.begin();
+  // DHT22
+  dht.begin();
+  //BH1750
+  lightSensor.begin();
+  //SI1145
+  uv.begin();
+  // Pluie
+  pinMode(PinPluie,INPUT);
+  //attachInterrupt(digitalPinToInterrupt(PinPluie),pluie,FALLING);
+  // MOD61016
+  Wire.begin();
+  mod1016.init(IRQ_ORAGE);
+  delay(2);
+  autoTuneCaps(IRQ_ORAGE);
+  delay(2);
+  //mod1016.setTuneCaps(6);
+  //delay(2);
+  mod1016.setOutdoors();
+  delay(2);
+  mod1016.setNoiseFloor(4); 	// Valeur par defaut 5
+  pinMode(IRQ_ORAGE, INPUT);
+  attachInterrupt(digitalPinToInterrupt(IRQ_ORAGE), orage, RISING);
+  mod1016.getIRQ();
+  // Envoi des premières mesures
+  infoMeteo();
+  envoiHTTP();
 }
 
+//-----------------------------------------------
+// Boucle principale
 //-----------------------------------------------
 void loop() {
   // OTA
@@ -163,5 +241,11 @@ void loop() {
   timer.run();
   // Serveur Web
   server.handleClient();
-  // Capteur de pluie
+    // MOD1016
+  if (detected) {
+    translateIRQ(mod1016.getIRQ());
+    detected = false;
+  }
+  // Capteur de pluie TODO
+  
 }
